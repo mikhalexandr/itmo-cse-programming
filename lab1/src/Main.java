309c449df28faa76944a26998a6c60fdfe493a6a
mikhalexandr <mikhalchenkov22@gmail.com>
2025-09-21 21:48:18 +0300

    feat: lab1

---
+++ lab1/src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] w = createW();
+        double[] x = createX();
+        double[][] s = computeMatrix(w, x);
+        printMatrix(s);
+    }
+
+    public static int[] createW() {
+        int[] w = new int[16];
+        for (int i = 0; i < 16; i++) {
+            w[i] = 20 - i;
+        }
+        return w;
+    }
+
+    public static double[] createX() {
+        double[] x = new double[17];
+        Random r = new Random();
+        for (int i = 0; i < 17; i++) {
+            x[i] = -6.0 + r.nextDouble() * 9.0;
+        }
+        return x;
+    }
+
+    public static double[][] computeMatrix(int[] w, double[] x) {
+        double[][] s = new double[w.length][x.length];
+        for (int i = 0; i < w.length; i++) {
+            for (int j = 0; j < x.length; j++) {
+                s[i][j] = computeElement(w[i], x[j]);
+            }
+        }
+        return s;
+    }
+
+    public static double computeElement(int w_i, double x_j) {
+        if (w_i == 12) {
+            return Math.cos(Math.tan(Math.atan((x_j - 1.5) / 9.0)));
+        } else if (w_i == 6 || w_i == 8 || w_i == 10 || w_i == 13 ||
+                w_i == 14 || w_i == 15 || w_i == 17 || w_i == 19) {
+            return Math.sin(
+                    Math.pow(Math.pow(x_j, (1 - x_j) / 1.0 / 4.0) / 2.0, Math.pow(3.0 / (x_j - 1), 2)));
+        } else {
+            double a = Math.pow(Math.tan(x_j), 3 * (4 - Math.atan((x_j - 1.5) / 9.0))) + 1;
+            double b = Math.pow(Math.pow((2 / 3.0) / (x_j - (3 / 4.0)), x_j) * (Math.asin((x_j - 1.5) / 9.0) - 1), 2);
+            return Math.atan(Math.cos(Math.pow(a, b)));
+        }
+    }
+
+    public static void printMatrix(double[][] s) {
+        for (int i = 0; i < s.length; i++) {
+            for (int j = 0; j < s[i].length; j++) {
+                System.out.printf("%8.3f", s[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
